ebd476c0588daf35e4b9b331185e9e7abf9127c7
Emil Enchev <emoen4ev@gmail.com>
2022-10-04 16:50:32 +0300

    Created all.html in templates\departments. Modified departments\views.py.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/04. URLs and VIews/departments_app/departments_app/departments/views.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/04. URLs and VIews/departments_app/departments_app/departments/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, redirect
 
 
 # def sample_view(request, *args, **kwargs):
@@ -7,13 +7,31 @@ from django.shortcuts import render
 #     print(f"kwargs= {kwargs}")
 
 
-def show_departments(request, *args, **kwargs):
-    # body = f"args= {args}, kwargs= {kwargs}"
-    body = f"path: {request.path}, args= {args}, kwargs= {kwargs}"
-    return HttpResponse(body)
-    # return HttpResponse()
+# Without render
+# def show_departments(request: HttpRequest, *args, **kwargs):
+#     print(request.method)
+#     print(request.GET)  # query params (?param1=value1&param2=value2)
+#     print(request.POST)  # body of HTTP request
+#     print(request.get_port())
+#     print(request.get_host())
+#     print(request.headers)
+#     # body = f"args= {args}, kwargs= {kwargs}"
+
+#     order_by = request.GET.get("order_by", "name")
+#     body = f"path: {request.path}, args= {args}, kwargs= {kwargs}, order_by: {order_by}"
+#     return HttpResponse(body)
+#     # return HttpResponse()
 
 
-def show_department_details(request, department_id):
+def show_departments(request: HttpRequest, *args, **kwargs):
+    context = {
+        "method": request.method,
+        "order_by": request.GET.get("order_by", "name"),
+    }
+
+    return render(request, "departments/all.html", context)
+
+
+def show_department_details(request: HttpRequest, department_id):
     body = f"path: {request.path}, id: {department_id}"
     return HttpResponse(body)
