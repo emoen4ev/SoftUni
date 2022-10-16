d926ca349769b14a13c58de8556c5bf391269583
Emil Enchev <emoen4ev@gmail.com>
2022-10-16 17:25:14 +0300

    Created simple 'view'('index' function) in web\views.py.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/urls.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/urls.py
@@ -1,10 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("templates_demos.web.urls"))
+    path("", include("templates_demos.web.urls")),
 ]
 
 
--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/views.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'SoftUni Homepage',
+    }
+
+    return render(request, '', context)
