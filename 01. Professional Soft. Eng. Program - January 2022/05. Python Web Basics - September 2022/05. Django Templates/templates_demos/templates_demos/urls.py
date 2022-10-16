21d15039744c1e327b1da4138bdb87a6be325983
Emil Enchev <emoen4ev@gmail.com>
2022-10-16 16:57:19 +0300

    Created empty tuple 'urlpatterns' in web\urls.py, included web.urls.py to urlpatterns in templates_demos\urls.py, deleted unnecessary comments.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/urls.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/urls.py
@@ -1,30 +1,17 @@
-"""templates_demos URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("templates_demos.web.urls"))
 ]
 
 '''
-1. Create new django app(web) to root templates_demos folder
-2. Move new django app(web) to templates_demos\templates_demos folder
-3. Add to installed_apps
-4. Create urls.py in the app
+1. Create new django app(named 'web') to root templates_demos folder
+2. Move new django app('web') to templates_demos\templates_demos folder
+3. Add 'web' app to INSTALLED_APPS in settings.py.
+4. Create urls.py in the new 'web' app
 5. Include app's urls.py in project's urls.py
 
 '''
