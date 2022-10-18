cb60e40252e06dedbfa4e4c5e8152a1568b8132a
Emil Enchev <emoen4ev@gmail.com>
2022-10-18 14:21:07 +0300

    Added code to nav.html. In tags.py module created inclusion tag for example named 'app_nav'. In index.html added code to 'Tag Nav'.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/templatetags/tags.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/templatetags/tags.py
@@ -15,3 +15,13 @@ def show_student_info(student: Student):
 @register.simple_tag(name='sample_tag')
 def sample_tag(*args, **kwargs):
     return ', '.join(str(x) for x in (list(args) + list(kwargs.items())))
+
+
+@register.inclusion_tag('tags/nav.html', name='app_nav')
+def generate_nav(*args):
+    context = {
+        'url_names': args,
+
+    }
+
+    return context
