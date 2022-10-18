02d156fb2d78ec99dbc6d612dd405f62c5fab3de
Emil Enchev <emoen4ev@gmail.com>
2022-10-18 21:25:14 +0300

    Added empty 'urlpatterns' in APP_NAME/urls.py files.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/06. Workshop Part 1/petstagram/petstagram/urls.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/06. Workshop Part 1/petstagram/petstagram/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
 '''
 After 'startapp APP_NAME':
 
-1. Create 'APP_NAME/urls.py.
+1. Create 'APP_NAME/urls.py with empty 'urlpatterns'(urlpatterns = (), list or tuple).
+2. Include 'APP_NAME/urls.py' into project's urls.py.
+3. Add 'APP_NAME' to 'INSTALLED_APPS' in settings.py.
+
 '''
