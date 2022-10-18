5d0ec4aed9e7a4cf0bcfa50ca1583069170d370f
Emil Enchev <emoen4ev@gmail.com>
2022-10-18 09:58:56 +0300

    Created 'app_style' filter in filters.py. Added example of use new 'app_style' filter in index.html.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/templatetags/filters.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/templatetags/filters.py
@@ -5,4 +5,11 @@ register = Library()
 
 @register.filter('odd')
 def get_odd(values):
+    # Use any python/django tools
+    # for example: Get objects from DB
     return [x for x in values if x % 2 == 1]
+
+
+@register.filter('app_style')
+def format_to_app_style(date):
+    return date.strftime('%Y/%m/%d %H:%M:%S')
