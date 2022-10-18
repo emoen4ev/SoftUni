d3c30e52741d9c3c5b7b7d3dfc7aef783b1fb8b2
Emil Enchev <emoen4ev@gmail.com>
2022-10-18 15:25:30 +0300

    In web\urls.py added path to 'about'.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/urls.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from templates_demos.web.views import index, redirect_to_home
+from templates_demos.web.views import index, redirect_to_home, about
 
 urlpatterns = (
     path('', index, name= 'index'),
     path("go-to-home/", redirect_to_home, name="redirect to home"),
+    path('about/', about, name='about'),
 
 )
