3119138593c7916a06d4a358c7f739f04f025587
Emil Enchev <emoen4ev@gmail.com>
2022-10-18 12:00:06 +0300

    Created 'sample_tag' for example in tags.py module. Added in index.html.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/templatetags/tags.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/05. Django Templates/templates_demos/templates_demos/web/templatetags/tags.py
@@ -10,3 +10,8 @@ register = Library()
 @register.simple_tag(name='student_info')
 def show_student_info(student: Student):
     return f'Hello, My name is {student.name} and I am {student.age}-years old.'
+
+
+@register.simple_tag(name='sample_tag')
+def sample_tag(*args, **kwargs):
+    return ', '.join(str(x) for x in (list(args) + list(kwargs.items())))
