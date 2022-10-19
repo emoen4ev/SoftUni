2473dce2ec439e41fadbf4a84a028a81a896258e
Emil Enchev <emoen4ev@gmail.com>
2022-10-19 19:43:13 +0300

    Added different templates in project templates folder.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/06. Workshop Part 1/petstagram/petstagram/urls.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/06. Workshop Part 1/petstagram/petstagram/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('petstagram.common.urls')),
     path('accounts/', include('petstagram.accounts.urls')),
-    path('common/', include('petstagram.common.urls')),
     path('pets/', include('petstagram.pets.urls')),
-    path('photos', include('petstagram.photos'))
+    path('photos', include('petstagram.photos.urls')),
 ]
 
 '''
