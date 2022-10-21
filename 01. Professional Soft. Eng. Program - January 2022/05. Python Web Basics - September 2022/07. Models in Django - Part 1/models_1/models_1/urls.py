4f1a5af374f80a9f6c70e852f0af85121d8fc509
Emil Enchev <emoen4ev@gmail.com>
2022-10-21 18:26:12 +0300

    Created django project 'models_1'. Deleted old django project 'models_demos_1'.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/urls.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/urls.py
@@ -1,4 +1,4 @@
-"""models_demos_1 URL Configuration
+"""models_1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
