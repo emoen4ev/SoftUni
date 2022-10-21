6a7f0ff1235ff0ba0639456a1fc11da22ffb44e5
Emil Enchev <emoen4ev@gmail.com>
2022-10-21 19:48:16 +0300

    Created 'Employee' class/model in models.py.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/web/models.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/web/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Employee(models.Model):
+    pass
+
+
+'''
+Django ORM (Object-relational mapping)
+'''
+
+Employee.objects.all()  # Select
+Employee.objects.create()  # Insert
+Employee.objects.filter()  # Select + Where
+Employee.objects.update()  # Update
