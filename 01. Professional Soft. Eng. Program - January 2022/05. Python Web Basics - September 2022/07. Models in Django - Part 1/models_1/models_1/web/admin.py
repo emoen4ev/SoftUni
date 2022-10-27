ea1e3f3ebd6a0dd9a13236c2ce1ad7f68fac0276
Emil Enchev <emoen4ev@gmail.com>
2022-10-27 20:30:34 +0300

    Created 'NullBlankDemo' class in models.py, register it in admin.py and performed migrations.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/web/admin.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/web/admin.py
@@ -1,3 +1,16 @@
+# admin.py
+
 from django.contrib import admin
 
-# Register your models here.
+from models_1.web.models import Employee, NullBlankDemo
+
+
+# The 'Employee' model is enabled in django admin
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NullBlankDemo)
+class NullBlankDemoAdmin(admin.ModelAdmin):
+    pass
--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/web/models.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/web/models.py
@@ -20,7 +20,9 @@ class Employee(models.Model):
         max_length=15,
     )
 
-    age = models.IntegerField()
+    age = models.IntegerField(
+        default=-1
+    )
 
     # int
     years_of_experience = models.IntegerField()
@@ -44,7 +46,42 @@ class Employee(models.Model):
         auto_now=True,  # optional
     )
 
-    email = models.EmailField()
+    email = models.EmailField(
+        # Adds 'UNIQUE' constraint
+        unique=True,
+    )
+
+    @property
+    def fullname(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        # self.id == self.pk
+        # return f'Id: {self.pk} / Name: {self.first_name} {self.last_name}' # without @property
+        return f'Id: {self.pk} / Name: {self.fullname}'  # used @property
+
+
+class NullBlankDemo(models.Model):
+    blank = models.IntegerField(
+        blank=True,
+        null=False,
+    )
+
+    null = models.IntegerField(
+        blank=False,
+        null=True,
+    )
+
+    blank_null = models.IntegerField(
+        blank=True, # Form validation
+        null=True,
+    )
+
+    default = models.IntegerField(
+        blank=False,
+        null=False,
+    )
+
 
 
 '''
