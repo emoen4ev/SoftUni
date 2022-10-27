31161fe6a363ba565d3ba71e3b51b1029a1bfc6f
Emil Enchev <emoen4ev@gmail.com>
2022-10-27 13:56:09 +0300

    Added examples in models.py. Performed а few migrations.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/web/models.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/07. Models in Django - Part 1/models_1/models_1/web/models.py
@@ -1,10 +1,46 @@
 from django.db import models
 
 
-# Create your models here.
+# models.py
+
+# Model fields == class attributes in Model classes
 
 class Employee(models.Model):
-    pass
+    # Var char(30) => strings with max length of 30 characters
+    first_name = models.CharField(
+        max_length=30,
+    )
+
+    last_name = models.CharField(
+        max_length=50,
+        null=True,
+    )
+
+    # int
+    years_of_experience = models.IntegerField()
+
+    # int > 0
+    years_of_experience_positive = models.PositiveIntegerField()
+
+    # Text => strings with unlimited length
+    review = models.TextField()
+
+    start_date = models.DateField() # This field is filled in manually,
+    # not automatically, unlike next two examples below
+
+    # This will be automatically set on creation
+    created_on = models.DateTimeField(
+        auto_now_add=True, # optional
+    )
+
+    # This will be automatically set on each 'save'/'update'
+    updated_on = models.DateTimeField(
+        auto_now=True, # optional
+    )
+
+    email = models.EmailField()
+
+
 
 
 '''
