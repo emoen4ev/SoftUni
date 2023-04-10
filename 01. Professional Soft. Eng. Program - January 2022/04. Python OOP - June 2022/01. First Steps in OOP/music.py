3a38141dc0c9ba0abaf4ef1f572269629471b025
Emil Enchev <emoen4ev@gmail.com>
2023-04-10 14:29:11 +0300

    Changes in the task condition.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/01. First Steps in OOP/music.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/01. First Steps in OOP/music.py
@@ -1,3 +1,16 @@
+"""
+Music
+
+Create a class named Music that receives title (string), artist (string), and lyrics (string) upon initialization.
+
+The class should also have two additional methods:
+•	The print_info() method should return the following: 'This is "{title}" from "{artist}"'
+•	The play() method should return the lyrics.
+
+Submit only the class in the judge system. Test your code with your own examples.
+"""
+
+
 class Music:
     def __init__(self, title, artist, lyrics):
         self.title = title
@@ -10,6 +23,22 @@ class Music:
     def play(self):
         return self.lyrics
 
+
 # song = Music("Title", "Artist", "Lyrics")
 # print(song.print_info())
 # print(song.play())
+
+'''
+Test Code:
+
+song = Music("Title", "Artist", "Lyrics")
+print(song.print_info())
+print(song.play())
+
+----------------------------------------------------------------
+
+Output:
+
+This is "Title" from "Artist"
+Lyrics
+'''
