d9e1048fe0d5cbf39de193b53762e72801329645
Emil Enchev <emoen4ev@gmail.com>
2023-04-10 19:01:58 +0300

    Fixed class attribute calling.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/03. Classes and Objects/glass.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/03. Classes and Objects/glass.py
@@ -1,3 +1,23 @@
+"""
+4.	Glass
+
+Create a class called Glass.
+
+Upon initialization, it will not receive any parameters.
+
+You must create an instance attribute called content which should be equal to 0.
+
+You should also create a class attribute called capacity which should be 250 ml.
+
+Create 3 instance methods:
+-	fill(ml) - fills the glass with the given milliliters if there is enough space in it
+and returns "Glass filled with {ml} ml", otherwise returns "Cannot add {ml} ml"
+-	empty() - empties the glass and returns "Glass is now empty"
+-	info() - returns info about the glass in the format "{space_left} ml left"
+
+"""
+
+
 # class Glass:
 #     capacity = 250
 #
@@ -5,7 +25,7 @@
 #         self.content = 0
 #
 #     def fill(self, ml):
-#         if ml <= self.capacity - self.content:
+#         if ml <= Glass.capacity - self.content:
 #             self.content += ml
 #             return f'Glass filled with {ml} ml'
 #         return f'Cannot add {ml} ml'
@@ -15,7 +35,7 @@
 #         return f'Glass is now empty'
 #
 #     def info(self):
-#         space_left = self.capacity - self.content
+#         space_left = Glass.capacity - self.content
 #         return f'{space_left} ml left'
 
 
@@ -27,7 +47,7 @@
 #         self.content = self.initial_content
 #
 #     def fill(self, ml):
-#         if ml <= self.capacity - self.content:
+#         if ml <= Glass.capacity - self.content:
 #             self.content += ml
 #             return f'Glass filled with {ml} ml'
 #         return f'Cannot add {ml} ml'
@@ -37,7 +57,7 @@
 #         return f'Glass is now empty'
 #
 #     def info(self):
-#         space_left = self.capacity - self.content
+#         space_left = Glass.capacity - self.content
 #         return f'{space_left} ml left'
 
 
@@ -46,7 +66,7 @@ class Glass:
     capacity = 250
 
     def __init__(self):
-        self.content = self.initial_content
+        self.content = Glass.initial_content
 
     def fill(self, ml):
         space_left = self.calculate_space_left()
@@ -65,13 +85,33 @@ class Glass:
         return f'{space_left} ml left'
 
     def calculate_space_left(self):
-        return self.capacity - self.content
+        return Glass.capacity - self.content
 
 
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
 
-# glass = Glass()
-# print(glass.fill(100))
-# print(glass.fill(200))
-# print(glass.empty())
-# print(glass.fill(200))
-# print(glass.info())
+'''
+Test Code:
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
+
+----------------------------------------------------------------
+
+Output:
+
+Glass filled with 100 ml
+Cannot add 200 ml
+Glass is now empty
+Glass filled with 200 ml
+50 ml left
+'''
