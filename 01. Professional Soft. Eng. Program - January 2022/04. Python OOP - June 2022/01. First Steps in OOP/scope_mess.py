1927c7f581c4ec15e8debcea784ae35408c10ea3
Emil Enchev <emoen4ev@gmail.com>
2023-04-10 11:52:37 +0300

    Changes to task condition ordering.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/01. First Steps in OOP/scope_mess.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/01. First Steps in OOP/scope_mess.py
@@ -1,28 +1,52 @@
-#  ------ original ----------------
+"""
+Scope Mess
 
-# x = "global"
-#
-# def outer():
-#     x = "local"
-#
-#     def inner():
-#         x = "nonlocal"
-#         print("inner:", x)
-#
-#     def change_global():
-#         x = "global: changed!"
-#
-#     print("outer:", x)
-#     inner()
-#     print("outer:", x)
-#     change_global()
-#
-# print(x)
-# outer()
-# print(x)
+Fix the code below, so it returns the expected output.
+Submit the fixed code in the judge system.
 
-# ---------------------------------------------
+------ original ----------------
 
+x = "global"
+
+def outer():
+    x = "local"
+
+    def inner():
+        x = "nonlocal"
+        print("inner:", x)
+
+    def change_global():
+        x = "global: changed!"
+
+    print("outer:", x)
+    inner()
+    print("outer:", x)
+    change_global()
+
+print(x)
+outer()
+print(x)
+"""
+
+'''
+Current Output:
+
+global
+outer: local
+inner: nonlocal
+outer: local
+global
+
+----------------------------------------------------------------
+
+Expected Output:
+
+global
+outer: local
+inner: nonlocal
+outer: nonlocal
+global: changed!
+'''
 
 x = "global"
 
@@ -48,15 +72,6 @@ def outer():
 print(x)
 outer()
 print(x)
-
-'''
-global
-outer: local
-inner: nonlocal
-outer: nonlocal
-global: changed!
-'''
-
 
 # def get_print_list_func(separator_char):
 #     def internal_print(ll):
