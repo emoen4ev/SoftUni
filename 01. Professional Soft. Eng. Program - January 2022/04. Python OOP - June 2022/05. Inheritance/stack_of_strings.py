83c16815b0631207aa0f97bd340dd7c62efd409f
Emil Enchev <emoen4ev@gmail.com>
2023-04-13 18:57:03 +0300

    Added task conditions.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/05. Inheritance/stack_of_strings.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/05. Inheritance/stack_of_strings.py
@@ -1,3 +1,17 @@
+"""
+6.	Stack of Strings
+
+Create a class Stack that can store only strings and has the following functionality:
+•	Instance attribute: data: list
+•	Method: push(element) – adds an element at the end of the stack
+•	Method: pop() – removes and returns the last element in the stack
+•	Method: top() - returns a reference to the topmost element of the stack
+•	Method: is_empty() - returns boolean True/False
+•	Override the string method to return the stack data in the format:
+"[{element(N)}, {element(N-1)} ... {element(0)}]"
+"""
+
+
 class Stack:
     def __init__(self):
         self.data = []
