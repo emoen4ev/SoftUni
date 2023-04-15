2468d4a33a5d3c8faf9f64d8e2399b63a5ca8e32
Emil Enchev <emoen4ev@gmail.com>
2023-04-15 19:50:01 +0300

    Added task conditions.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/09. Static and Class Methods/calculator.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/09. Static and Class Methods/calculator.py
@@ -1,3 +1,13 @@
+"""
+1.	Calculator
+
+Create a class called Calculator that has the following static methods:
+•	add(*args) - sums all the arguments passed to the function and returns the result
+•	multiply(*args) - multiplies all the numbers and returns the result
+•	divide(*args) - divides all the numbers (starting from the first one) and returns the result
+•	subtract(*args) - subtracts all the numbers (starting from the first one) and returns the result
+"""
+
 from functools import reduce
 
 
@@ -23,3 +33,21 @@ print(Calculator.add(5, 10, 4))
 print(Calculator.multiply(1, 2, 3, 5))
 print(Calculator.divide(100, 2))
 print(Calculator.subtract(90, 20, -50, 43, 7))
+
+'''
+Test Code:
+
+print(Calculator.add(5, 10, 4))
+print(Calculator.multiply(1, 2, 3, 5))
+print(Calculator.divide(100, 2))
+print(Calculator.subtract(90, 20, -50, 43, 7))
+
+----------------------------------------------------------------
+
+Output:
+
+19
+30
+50.0
+70
+'''
