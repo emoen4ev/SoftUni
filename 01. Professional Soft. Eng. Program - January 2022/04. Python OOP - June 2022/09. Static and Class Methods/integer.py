c3f30230a3ce9cf5e255d8001ea4386be9f1301c
Emil Enchev <emoen4ev@gmail.com>
2023-04-15 23:31:33 +0300

    Added task conditions.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/09. Static and Class Methods/integer.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/09. Static and Class Methods/integer.py
@@ -1,4 +1,15 @@
-# from math import floor
+"""
+3.	Integer
+
+Create a class called Integer.
+Upon initialization, it should receive a single parameter value (int).
+It should have 3 additional methods:
+•	from_float(float_value) - creates a new instance by flooring the provided floating number.
+If the value is not a float, return a message "value is not a float"
+•	from_roman(value) - creates a new instance by converting the roman number (as string) to an integer
+•	from_string(value) - creates a new instance by converting the string to an integer
+(if the value cannot be converted, return a message "wrong type")
+"""
 
 
 class Integer:
@@ -17,7 +28,7 @@ class Integer:
 
     @classmethod
     def from_roman(cls, value):
-        roman_numerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         result = 0
         for i, char in enumerate(value):
             if (i + 1) == len(value) or roman_numerals[char] >= roman_numerals[value[i + 1]]:
@@ -44,3 +55,25 @@ print(second_num.value)
 
 print(Integer.from_float("2.6"))
 print(Integer.from_string(2.6))
+
+'''
+Test Code:
+
+first_num = Integer(10)
+print(first_num.value)
+
+second_num = Integer.from_roman("IV")
+print(second_num.value)
+
+print(Integer.from_float("2.6"))
+print(Integer.from_string(2.6))
+
+----------------------------------------------------------------
+
+Output:
+
+10
+4
+value is not a float
+wrong type
+'''
