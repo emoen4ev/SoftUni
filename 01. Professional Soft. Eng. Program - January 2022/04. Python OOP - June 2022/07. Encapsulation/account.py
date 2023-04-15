3f5a090ab1f3f3a0904f144bed0602e91834f644
Emil Enchev <emoen4ev@gmail.com>
2023-04-15 16:51:41 +0300

    Added task conditions.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/07. Encapsulation/account.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/07. Encapsulation/account.py
@@ -1,3 +1,16 @@
+"""
+5.	Account
+
+Create a class called Account.
+Upon initialization, it should receive an id, a balance, and a pin (all numbers).
+The pin and the id should be private instance attributes, and the balance should be a public attribute.
+Create two public instance methods:
+•	get_id(pin) - if the given pin is correct, return the id, otherwise, return "Wrong pin"
+•	change_pin(old_pin, new_pin) - if the old pin is correct,
+change it to the new one and return "Pin changed", otherwise return "Wrong pin"
+"""
+
+
 class Account:
     def __init__(self, id: int, balance: int, pin: int):
         self.__id = id
@@ -16,7 +29,15 @@ class Account:
         return 'Wrong pin'
 
 
+account = Account(8827312, 100, 3421)
+print(account.get_id(1111))
+print(account.get_id(3421))
+print(account.balance)
+print(account.change_pin(2212, 4321))
+print(account.change_pin(3421, 1234))
 
+'''
+Test Code:
 
 account = Account(8827312, 100, 3421)
 print(account.get_id(1111))
@@ -24,3 +45,14 @@ print(account.get_id(3421))
 print(account.balance)
 print(account.change_pin(2212, 4321))
 print(account.change_pin(3421, 1234))
+
+----------------------------------------------------------------
+
+Output:
+
+Wrong pin
+8827312
+100
+Wrong pin
+Pin changed
+'''
