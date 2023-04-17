d056c02ce4541c18c3e6c763b375cc6fe99cde6c
Emil Enchev <emoen4ev@gmail.com>
2023-04-17 16:09:50 +0300

    Added task solution.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/11. Polymorphism and Abstraction/image_area.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/11. Polymorphism and Abstraction/image_area.py
@@ -1,3 +1,13 @@
+"""
+2.	ImageArea
+
+Create a class called ImageArea which will store the width and the height of an image.
+Create a method called get_area() which will return the area of the image.
+We have also to implement all the magic methods for comparison of two image areas (>, >=, <, <=, ==, !=),
+which will compare their areas.
+"""
+
+
 class ImageArea:
     def __init__(self, w, h):
         self.width = w
@@ -42,3 +52,38 @@ class ImageArea:
 # a3 = ImageArea(8, 9)
 # print(a1 <= a2)
 # print(a1 < a3)
+
+'''
+Test Code:
+
+a1 = ImageArea(7, 10)
+a2 = ImageArea(35, 2)
+a3 = ImageArea(8, 9)
+print(a1 == a2)
+print(a1 != a3)
+
+a1 = ImageArea(7, 10)
+a2 = ImageArea(35, 2)
+a3 = ImageArea(8, 9)
+print(a1 != a2)
+print(a1 >= a3)
+
+a1 = ImageArea(7, 10)
+a2 = ImageArea(35, 2)
+a3 = ImageArea(8, 9)
+print(a1 <= a2)
+print(a1 < a3)
+
+----------------------------------------------------------------
+
+Output:
+
+True
+True
+
+False
+False
+
+True
+True
+'''
