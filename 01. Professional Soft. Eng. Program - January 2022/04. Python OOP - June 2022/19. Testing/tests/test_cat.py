d414922bcbc9c92c944f49d691f9d7d58a3c45fa
Emil Enchev <emoen4ev@gmail.com>
2023-04-18 00:51:12 +0300

    Added task condition.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/19. Testing/tests/test_cat.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/19. Testing/tests/test_cat.py
@@ -1,3 +1,17 @@
+"""
+2.	Test Cat
+
+Create a class CatTests
+In judge you need to submit just the CatTests class, with the unittest module imported.
+
+Create the following tests:
+•	Cat's size is increased after eating
+•	Cat is fed after eating
+•	Cat cannot eat if already fed, raises an error
+•	Cat cannot fall asleep if not fed, raises an error
+•	Cat is not sleepy after sleeping
+"""
+
 import unittest
 from unittest import TestCase
 
