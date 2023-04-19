368c48df4110190a7ac642adc70f6eed8402e648
Emil Enchev <emoen4ev@gmail.com>
2023-04-19 18:26:49 +0300

    Added task description. Fixed solution.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/02. First Steps in OOP - Exercise/programmer.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/02. First Steps in OOP - Exercise/programmer.py
@@ -1,3 +1,26 @@
+"""
+7.	Programmer
+
+Create a class called Programmer.
+Upon initialization, it should receive name (string), language (string), skills (integer).
+
+The class should have two methods:
+-	watch_course(course_name, language, skills_earned)
+    o	If the programmer's language is the same as the one on the course, increase his skills with the given amount
+        and return a message "{name} watched {course_name}".
+    o	Otherwise return "{name} does not know {language}".
+
+-	change_language(new_language, skills_needed)
+    o	If the programmer has the skills and the new language is not the same as his,
+        change his language to the new one and return "{name} switched from {previous_language} to {new_language}".
+    o	If the programmer has the skills, but the given language is equal to his return "{name} already knows {language}".
+    o	In the last case, the programmer does not have enough skills,
+    so return "{name} needs {needed_skills} more skills" and do not change his language.
+
+Submit only the class in the judge system.
+"""
+
+
 class Programmer:
     def __init__(self, name: str, language: str, skills: int):
         self.name = name
@@ -5,22 +28,21 @@ class Programmer:
         self.skills = skills
 
     def watch_course(self, course_name: str, language: str, skills_earned: int):
-        if self.language != language:
-            return f'{self.name} does not know {language}'
-        self.skills += skills_earned
-        return f'{self.name} watched {course_name}'
+        if self.language == language:
+            self.skills += skills_earned
+            return f'{self.name} watched {course_name}'
+        return f'{self.name} does not know {language}'
 
     def change_language(self, new_language: str, skills_needed: int):
-        if self.skills < skills_needed:
+        if skills_needed > self.skills:
             needed_skills = skills_needed - self.skills
             return f'{self.name} needs {needed_skills} more skills'
-        elif new_language == self.language:
+        if new_language == self.language:
             return f'{self.name} already knows {new_language}'
-        else:
-            previous_language = self.language
-            self.language = new_language
-            return f'{self.name} switched from {previous_language} to {new_language}'
 
+        old_language = self.language
+        self.language = new_language
+        return f'{self.name} switched from {old_language} to {new_language}'
 
 
 programmer = Programmer("John", "Java", 50)
@@ -30,3 +52,26 @@ print(programmer.change_language("Python", 100))
 print(programmer.watch_course("Java: zero to hero", "Java", 50))
 print(programmer.change_language("Python", 100))
 print(programmer.watch_course("Python Masterclass", "Python", 84))
+
+'''
+Test Code:
+
+programmer = Programmer("John", "Java", 50)
+print(programmer.watch_course("Python Masterclass", "Python", 84))
+print(programmer.change_language("Java", 30))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Java: zero to hero", "Java", 50))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Python Masterclass", "Python", 84))
+
+----------------------------------------------------------------
+
+Output:
+
+John does not know Python
+John already knows Java
+John needs 50 more skills
+John watched Java: zero to hero
+John switched from Java to Python
+John watched Python Masterclass
+'''
