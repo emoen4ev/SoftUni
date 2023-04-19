5c82775ab2773273ddccf17aeb7959f07e38d44d
Emil Enchev <emoen4ev@gmail.com>
2023-04-19 14:07:42 +0300

    Added task description.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/02. First Steps in OOP - Exercise/hero.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/02. First Steps in OOP - Exercise/hero.py
@@ -1,3 +1,16 @@
+"""
+2.	Hero
+
+Create a class called Hero.
+Upon initialization, it should receive a name (string) and health (number).
+Create two additional methods:
+•	defend(damage) - reduce the given damage from the hero's health:
+        -if the health become 0 or less, set it to 0 and return "{name} was defeated"
+
+•	heal(amount) - increase the health of the hero with the given amount
+"""
+
+
 class Hero:
     def __init__(self, name: str, health: int):
         self.name = name
@@ -12,8 +25,27 @@ class Hero:
     def heal(self, amount):
         self.health += amount
 
+
 hero = Hero("Peter", 100)
 print(hero.defend(50))
 hero.heal(50)
 print(hero.defend(99))
 print(hero.defend(1))
+
+'''
+Test Code:
+
+hero = Hero("Peter", 100)
+print(hero.defend(50))
+hero.heal(50)
+print(hero.defend(99))
+print(hero.defend(1))
+
+----------------------------------------------------------------
+
+Output:
+
+None
+None
+Peter was defeated
+'''
