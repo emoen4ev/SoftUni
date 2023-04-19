8c6364077efc0c43e5959f09f6295c796ec683b1
Emil Enchev <emoen4ev@gmail.com>
2023-04-19 17:12:46 +0300

    Added task description.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/02. First Steps in OOP - Exercise/steam_user.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/02. First Steps in OOP - Exercise/steam_user.py
@@ -1,3 +1,24 @@
+"""
+6.	Steam User
+
+Create a class called SteamUser.
+Upon initialization, it should receive a username (string) and games (list).
+It should also have an attribute called played_hours (0 by default).
+
+Add three methods to the class:
+-	play(game, hours)
+o	If the game is in the game list, increase the played_hours by the given hours
+and return "{username} is playing {game}"
+o	Otherwise, return "{game} is not in library"
+-	buy_game(game)
+o	If the game is not in the game list, add it and return "{username} bought {game}"
+o	Otherwise, return "{game} is already in your library"
+-	status() - returns the following:
+    "{username} has {games_count} games. Total play time: {played_hours}"
+Submit only the class in the judge system.
+"""
+
+
 class SteamUser:
     def __init__(self, username: str, games: list):
         self.username = username
@@ -27,3 +48,26 @@ print(user.buy_game("CS:GO"))
 print(user.buy_game("Oxygen Not Included"))
 print(user.play("Oxygen Not Included", 6))
 print(user.status())
+
+'''
+Test Code:
+
+user = SteamUser("Peter", ["Rainbow Six Siege", "CS:GO", "Fortnite"])
+print(user.play("Fortnite", 3))
+print(user.play("Oxygen Not Included", 5))
+print(user.buy_game("CS:GO"))
+print(user.buy_game("Oxygen Not Included"))
+print(user.play("Oxygen Not Included", 6))
+print(user.status())
+
+----------------------------------------------------------------
+
+Output:
+
+Peter is playing Fortnite
+Oxygen Not Included is not in library
+CS:GO is already in your library
+Peter bought Oxygen Not Included
+Peter is playing Oxygen Not Included
+Peter has 4 games. Total play time: 9
+'''
