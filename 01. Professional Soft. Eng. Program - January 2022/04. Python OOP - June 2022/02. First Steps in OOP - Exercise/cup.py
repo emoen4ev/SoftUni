c5f0f2685b772fd31bf6b2d83a00906d2ee48671
Emil Enchev <emoen4ev@gmail.com>
2023-04-19 16:27:40 +0300

    Added task description.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/02. First Steps in OOP - Exercise/cup.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/02. First Steps in OOP - Exercise/cup.py
@@ -1,21 +1,23 @@
-# class Cup:
-#     def __init__(self, size: int, quantity: int):
-#         self.size = size
-#         self.quantity = quantity
-#
-#     def fill(self, quantity):
-#         if self.quantity + quantity <= self.size:
-#             self.quantity += quantity
-#
-#     def status(self):
-#         return self.size - self.quantity
+"""
+4.	Cup
+
+Create a class called Cup.
+Upon initialization, it should receive size (integer) and quantity (an integer representing how much liquid is in it).
+The class should have two methods:
+•	fill(quantity) that will increase the amount of liquid in the cup with the given quantity
+(if there is space in the cup, otherwise ignore).
+•	status() that will return the amount of free space left in the cup.
+
+Submit only the class in the judge system. Do not forget to test your code.
+"""
+
 
 class Cup:
     def __init__(self, size: int, quantity: int):
         self.size = size
         self.quantity = quantity
 
-    def fill(self, quantity):
+    def fill(self, quantity: int):
         if self.status() >= quantity:
             self.quantity += quantity
 
@@ -23,9 +25,22 @@ class Cup:
         return self.size - self.quantity
 
 
+cup = Cup(100, 50)
+print(cup.status())
+cup.fill(40)
+cup.fill(20)
+print(cup.status())
+
+'''
+Test Code:
 
 cup = Cup(100, 50)
 print(cup.status())
 cup.fill(40)
 cup.fill(20)
 print(cup.status())
+
+Output:
+50
+10
+'''
