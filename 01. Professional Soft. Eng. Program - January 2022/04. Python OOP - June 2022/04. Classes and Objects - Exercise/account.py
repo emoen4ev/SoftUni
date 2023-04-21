4b80927d9ad122fa35507210135b7f5fd1dced5c
Emil Enchev <emoen4ev@gmail.com>
2023-04-21 15:11:41 +0300

    Fixed solution.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/04. Classes and Objects - Exercise/account.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/04. Classes and Objects - Exercise/account.py
@@ -16,7 +16,7 @@ and returns the new balance. Otherwise, return "Amount exceeded balance"
 
 
 class Account:
-    def __init__(self, id: int, name: str, balance=0):
+    def __init__(self, id: int, name: str, balance: int = 0):
         self.id = id
         self.name = name
         self.balance = balance
@@ -27,7 +27,7 @@ class Account:
 
     def debit(self, amount: int):
         if amount > self.balance:
-            return 'Amount exceeded balance'
+            return f'Amount exceeded balance'
 
         self.balance -= amount
         return self.balance
@@ -36,11 +36,10 @@ class Account:
         return f'User {self.name} with account {self.id} has {self.balance} balance'
 
 
-# account = Account(1234, "George", 1000)
-# print(account.credit(500))
-# print(account.debit(1500))
-# print(account.info())
-
+account = Account(1234, "George", 1000)
+print(account.credit(500))
+print(account.debit(1500))
+print(account.info())
 
 account = Account(5411256, "Peter")
 print(account.debit(500))
