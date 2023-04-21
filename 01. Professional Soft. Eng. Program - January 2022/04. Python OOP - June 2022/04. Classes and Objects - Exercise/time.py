269010ee5fff51e93f22a43e52baade22048bacb
Emil Enchev <emoen4ev@gmail.com>
2023-04-21 14:16:51 +0300

    Added task description.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/04. Classes and Objects - Exercise/time.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/04. Classes and Objects - Exercise/time.py
@@ -1,3 +1,20 @@
+"""
+2.	Time
+
+Create a class called Time.
+Upon initialization, it should receive hours, minutes, and seconds (integers).
+The class should also have class attributes max_hours equal to 23, max_minutes equal to 59, and max_seconds equal to 59.
+
+You should also create 3 additional instance methods:
+-	set_time(hours, minutes, seconds) - updates the time with the new values
+
+-	get_time() - returns "{hh}:{mm}:{ss}"
+
+-	next_second() - updates the time with one second (use the class attributes for validation)
+and returns the new time (use the get_time() method)
+"""
+
+
 class Time:
     max_hours = 23
     max_minutes = 59
@@ -8,7 +25,7 @@ class Time:
         self.minutes = minutes
         self.seconds = seconds
 
-    def set_time(self, hours: int, minutes: int, seconds: int ):
+    def set_time(self, hours: int, minutes: int, seconds: int):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
@@ -38,3 +55,22 @@ class Time:
 #
 # time = Time(23, 59, 59)
 # print(time.next_second())
+
+'''
+Test Code:
+
+time = Time(9, 30, 59)
+print(time.next_second())
+time = Time(10, 59, 59)
+print(time.next_second())
+time = Time(23, 59, 59)
+print(time.next_second())
+
+----------------------------------------------------------------
+
+Output:
+
+09:31:00
+11:00:00
+00:00:00
+'''
