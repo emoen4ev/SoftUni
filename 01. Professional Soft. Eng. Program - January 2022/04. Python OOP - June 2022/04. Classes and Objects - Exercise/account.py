5608c9bbe9e117b04a6940eaacfd224be9c7384a
Emil Enchev <emoen4ev@gmail.com>
2023-04-21 14:54:38 +0300

    Added task description.

--- 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/04. Classes and Objects - Exercise/account.py
+++ 01. Professional Soft. Eng. Program - January 2022/04. Python OOP - June 2022/04. Classes and Objects - Exercise/account.py
@@ -1,3 +1,20 @@
+"""
+3.	Account
+
+Create a class called Account.
+
+Upon initialization, it should receive an id (number), a name (string), and a balance (integer; optional; 0 by default).
+
+The class should also have 3 additional instance methods:
+-	credit(amount) - adds the amount to the balance and returns the new balance
+
+-	debit(amount) - if the amount is less than or equal to the balance, reduces the balance by the amount
+and returns the new balance. Otherwise, return "Amount exceeded balance"
+
+-	info() - returns "User {name} with account {id} has {balance} balance"
+"""
+
+
 class Account:
     def __init__(self, id: int, name: str, balance=0):
         self.id = id
@@ -30,3 +47,31 @@ print(account.debit(500))
 print(account.credit(1000))
 print(account.debit(500))
 print(account.info())
+
+'''
+Test Code:
+
+account = Account(1234, "George", 1000)
+print(account.credit(500))
+print(account.debit(1500))
+print(account.info())
+
+account = Account(5411256, "Peter")
+print(account.debit(500))
+print(account.credit(1000))
+print(account.debit(500))
+print(account.info())
+
+----------------------------------------------------------------
+
+Output:
+
+1500
+0
+User George with account 1234 has 0 balance
+
+Amount exceeded balance
+1000
+500
+User Peter with account 5411256 has 500 balance
+'''
